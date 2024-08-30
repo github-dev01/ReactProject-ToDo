8f1b40438cb8737c24da69cb23122c3ac8bdce92
github-dev01 <devpathak108@outlook.com>
2024-08-30 15:07:17 +0530

    finish with decent functionality and ui

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// https://youtu.be/gyQyk80_upM
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
